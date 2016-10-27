6ccbfe4c3253811d03bb7420105d2d62d8c4f612
Zireael07 <zi@mail.com>
2016-10-27 10:54:50 +0200

    get rid of the warning popup on launch

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     io.stdout:setvbuf("no")
     t.identity = nil                   -- The name of the save directory (string)
-    t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
+    t.version = "0.10.0"                -- The LÖVE version this game was made for (string)
     --t.console = true                -- Attach a console (boolean, Windows only)
     
     t.window.title = "Untitled"        -- The window title (string)
